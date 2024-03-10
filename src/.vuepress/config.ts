14158fd5e55d1237f6102f41b0f3d6a862f98f87
cbq-windows <2024cbq@gmail.com>
2024-03-10 12:14:44 +0800

    fix: base url

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -2,7 +2,7 @@ import { defineUserConfig } from "vuepress";
 import theme from "./theme.js";
 
 export default defineUserConfig({
-  base: "/",
+  base: "/vue-hope-template-cbq/",
 
   lang: "zh-CN",
   title: "文档演示",
