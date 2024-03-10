e91187f9f0a7ef2e8b48ff04b1f6eec123e73cd3
cbq-ubuntu <2024cbq@gmail.com>
2024-03-10 17:49:27 +0800

    feat: base url

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -1,15 +1,12 @@
-import { defineUserConfig } from "vuepress";
+import {defineUserConfig} from "vuepress";
 import theme from "./theme.js";
 
 export default defineUserConfig({
-  base: "/vue-hope-template-cbq/",
+    base: "/vue-hope-template-cbq/",
 
-  lang: "zh-CN",
-  title: "文档演示",
-  description: "vuepress-theme-hope 的文档演示",
+    lang: "zh-CN",
+    title: "Vue-Hope-Template-Cbq",
+    description: "vue-hope-template-cbq",
 
-  theme,
-
-  // 和 PWA 一起启用
-  // shouldPrefetch: false,
+    theme,
 });
